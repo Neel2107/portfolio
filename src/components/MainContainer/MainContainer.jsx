0af58692894e1ddd4d0751c9445452d0e11bb644
Neel <neelpatel805804@gmail.com>
2023-04-01 02:51:32 +0530

    main page working

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -31,17 +31,19 @@ const MainContainer = () => {
         <span>Hello, </span>
         My name is
       </span>
-      <span>
+      <span className='name'>
         Neel Patel
       </span>
-      <span>I'm a self taught web developer</span>
+      <span className='subtitle'>I'm a self taught web developer</span>
       <p className='personal-info'>
         Software enginner who specialises in producing top-notch digital experiences. Right now i'm focusing on creating responsive websites.
       </p>
 
     <a href="/" className='view-work'>Look at my work!</a>
       </div>
-      <div className='gifContainer'></div>
+      <div className='gifContainer'>
+        <img className='main-img' src="./coder.gif" alt="" />
+      </div>
     </div>
   )
 }

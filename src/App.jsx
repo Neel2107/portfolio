fd310f59120ebec321302a4b5c322e784267ffa9
Neel <neelpatel805804@gmail.com>
2023-04-01 13:59:13 +0530

    first section working with blob

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import MainContainer from "./components/MainContainer/MainContainer";
 import Navbar from "./components/Navbar/Navbar";
 
+import "./index.css"
+
 function App() {
+
+  const blob = document.getElementById("blob");
+
+  window.onpointermove = event => { 
+    const { clientX, clientY } = event;
+    
+    blob.animate({
+      left: `${clientX}px`,
+      top: `${clientY}px`
+    }, { duration: 200, fill: "forwards" });
+  }
   return (
     <div className="App">
+         <div id="blur"></div>
+         <div id="blob"></div>
       <Navbar />
       <MainContainer/>
+   
     </div>
   );
 }

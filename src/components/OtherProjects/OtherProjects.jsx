f3657e6c9873bd2f3178344696e1b20f40ff9f77
Neel <neelpatel805804@gmail.com>
2023-10-10 22:29:09 +0530

    jokebox project added

--- src/components/OtherProjects/OtherProjects.jsx
+++ src/components/OtherProjects/OtherProjects.jsx
@@ -197,6 +197,54 @@ const OtherProjects = () => {
             </div>
           </div>
         </div>
+        <div id="card" className="">
+          <div className="container">
+            <div className="upperPart">
+              <a href="https://github.com/Neel2107/JokeBox.git">
+                <img className="cardIcon" src="./folder.svg" alt="" />
+              </a>
+              <div
+                style={{
+                  width: "100%",
+                  alignItems: "flex-end",
+                  display: "flex",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <a href="https://github.com/Neel2107/JokeBox.git">
+                  {/* <img className="linkIcon" src="./link.png" alt="" /> */}
+                  <svg
+            // style={{ width: "20px", padding: "5px" }}
+            xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            strokeLinecap="round"
+            className=" linkIcon icons"
+          >
+            {/* <title>GitHub</title> */}
+            <path className="pathHover" d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+          </svg>
+                </a>
+              </div>
+            </div>
+            <div className="cardDetailContainer">
+              <a href="https://github.com/Neel2107/JokeBox.git" className="projectName">
+            JokeBox
+              </a>
+              <p className="projectDesc">
+              Designed and developed an engaging web app featuring a sleek, modern interface, delivering a continuous stream of humor with a single click.
+</p>
+              <div className="techContainer">
+                <div className="techUsed">React JS</div>
+                <div className="techUsed">API</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

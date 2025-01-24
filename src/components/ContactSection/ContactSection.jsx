b0fdd93cbfa306321eef516efb7ca9b080dba429
Neel <neelx2107@gmail.com>
2025-01-24 20:26:37 +0530

    chore: lucide-react install

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { Github, Instagram, Linkedin, Mail, Twitter } from "lucide-react";
 import React from "react";
 
 const ContactSection = () => {
@@ -12,28 +13,28 @@ const ContactSection = () => {
       href: "https://www.linkedin.com/in/neelpatel2107/",
       borderColor: "rgb(46, 166, 86)",
       shadowColor: "rgb(46, 166, 86)",
-      path: "M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z M2 9h4v12H2z M4 2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"
+      icon: Linkedin
     },
     {
       name: "GitHub",
       href: "https://github.com/Neel2107",
       borderColor: "rgba(255, 180, 0, 0.51)",
       shadowColor: "rgba(255, 180, 0, 0.51)",
-      path: "M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+      icon: Github
     },
     {
       name: "Instagram",
       href: "https://www.instagram.com/21_neeel/",
       borderColor: "rgba(219, 54, 205, 0.882)",
       shadowColor: "rgba(219, 54, 205, 0.882)",
-      path: "M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z M17.5 6.5h.01"
+      icon: Instagram
     },
     {
       name: "Twitter",
       href: "https://twitter.com/notneeel",
       borderColor: "rgba(0, 128, 255, 0.51)",
       shadowColor: "rgba(0, 128, 255, 0.51)",
-      path: "M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+      icon: Twitter
     }
   ];
 
@@ -74,9 +75,7 @@ const ContactSection = () => {
             viewport={{ once: true }}
             transition={{ duration: 0.6, delay: 0.4 }}
           >
-            Although I am currently new to the sector, based on my projects and
-            Intership experiences. I am delighted to begin a new chapter as a developer for
-            the company.
+            With my extensive experience in mobile and web development, including React Native and NestJS, I am excited to bring my skills to new challenges and contribute to impactful projects as a developer for the company.
           </motion.p>
 
           <motion.div
@@ -100,18 +99,10 @@ const ContactSection = () => {
                     boxShadow: `${social.shadowColor} 1px 5px 40px`
                   }}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    role="img"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1"
-                    strokeLinecap="round"
-                    className="md:tw-w-10 md:tw-h-10 tw-w-4 tw-h-4"
-                  >
-                    <path d={social.path}></path>
-                  </svg>
+                  {React.createElement(social.icon, {
+                    className: "md:tw-w-10 md:tw-h-10 tw-w-4 tw-h-4",
+                    strokeWidth: 1
+                  })}
                 </div>
               </motion.a>
             ))}
@@ -119,11 +110,11 @@ const ContactSection = () => {
 
           <motion.a
             href="https://mail.google.com/mail/u/0/#inbox?compose=new"
-            className="tw-border tw-border-[#89bbfe] tw-rounded-lg tw-text-[hsla(0,0%,78%,.68)] tw-text-sm tw-mt-5 tw-py-4 tw-px-12 tw-text-center tw-no-underline tw-w-max hover:tw-bg-[rgba(0,255,255,0.1)] hover:tw-shadow-[0_5px_20px_3px_#89bcfe43] hover:tw-text-white tw-transition-all tw-duration-500"
+            className="tw-border tw-border-[#89bbfe] tw-rounded-lg tw-text-[hsla(0,0%,78%,.68)] tw-text-sm tw-mt-5 tw-py-4 tw-px-12 tw-text-center tw-no-underline tw-w-max hover:tw-bg-[rgba(0,255,255,0.1)] hover:tw-shadow-[0_5px_20px_3px_#89bcfe43] hover:tw-text-white tw-transition-all tw-duration-500 tw-flex tw-items-center tw-gap-2"
             whileHover={{ scale: 1.05 }}
             transition={{ type: "spring", stiffness: 400, damping: 10 }}
           >
-            Say Hello!
+            Say Hello! <Mail className="tw-w-4 tw-h-4" />
           </motion.a>
 
           <motion.span

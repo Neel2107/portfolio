c2b1cfdd8a164689d3164f2a21c6e8746daabe09
Neel <neelpatel805804@gmail.com>
2025-10-02 16:59:34 +0530

    feat: theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
+import { ThemeProvider } from "@/components/common/ThemeProvider";
 import { LenisProvider } from "@/contexts/AnimationContext";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Neel - Portfolio",
@@ -25,12 +15,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <LenisProvider>
-          {children}
-        </LenisProvider>
+      <body className={`font-hanken-grotesk antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <LenisProvider>{children}</LenisProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
